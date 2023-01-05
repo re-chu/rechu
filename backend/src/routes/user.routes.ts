a97b5005faad71e5e23962cf2e2b9a93f549afaa
strombreeding <speaker1403@gmail.com>
2023-01-05 14:08:16 +0900

    FIX: 유저업데이트에 news 추가

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -190,6 +190,7 @@ userRoute.patch("/individuals", tokenValidator, async (req, res, next) => {
   const avatarUrl = req.body.avatarUrl;
   const working = req.body.working;
   const chance = req.body.chance;
+  const news = req.body.news;
   // const avatarUrl = req.body.avatarUrl;
   const toUpdate = {
     ...(password && { password }),
@@ -198,10 +199,14 @@ userRoute.patch("/individuals", tokenValidator, async (req, res, next) => {
     ...(gitHubUrl && { gitHubUrl }),
     ...(working && { working }),
     ...(chance && { chance }),
+    ...(news && { news }),
   };
   // 0값으로 들어오면 위 코드로 잡히지 않음.
   if (req.body.working === 0) {
     toUpdate.working = 0;
+  }
+  if (req.body.news === 0) {
+    toUpdate.news = 0;
   }
   console.log("업데이트 할 것들", toUpdate);
   try {
