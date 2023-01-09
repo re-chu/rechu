c64aeb2654914c0fc3ecfcacd5f26b7ab1e91964
strombreeding <speaker1403@gmail.com>
2023-01-09 20:54:36 +0900

    socket status

--- frontend/src/services/socket.ts
+++ frontend/src/services/socket.ts
@@ -1,6 +1,7 @@
 import io from 'socket.io-client';
-const BASE_URL = 'https://rechu.jinytree.shop';
+// const BASE_URL = 'https://rechu.jinytree.shop';
 // const BASE_URL = 'localhost:8080';
+const BASE_URL = 'localhost:6000';
 
 const socket = io(`${BASE_URL}`, { transports: ['websocket'] });
 
