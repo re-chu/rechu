280779b54a90e88c77e71c68c06a7327988639c5
strombreeding <speaker1403@gmail.com>
2023-01-09 20:55:53 +0900

    socket

--- frontend/src/services/socket.ts
+++ frontend/src/services/socket.ts
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
-// const BASE_URL = 'https://rechu.jinytree.shop';
+// const BASE_URL = 'rechu.jinytree.shop';
 // const BASE_URL = 'localhost:8080';
-const BASE_URL = 'localhost:6000';
+const BASE_URL = '/';
 
 const socket = io(`${BASE_URL}`, { transports: ['websocket'] });
 
