e43fcf41234c6441c102273cbb829ff5eace4be0
이진희 <speaker1403@gmail.com>
2023-01-10 00:18:54 +0900

    Add socket event

--- backend/src/socket/chat.ts
+++ backend/src/socket/chat.ts
@@ -1,18 +1,19 @@
-import { Socket } from 'socket.io';
-import wsServer from '../wsServer';
+import { Socket } from "socket.io";
+import wsServer from "../wsServer";
 
-const chatSocket = wsServer.of('/chat');
-chatSocket.on('connection', (socket: Socket) => {
-   console.log('채팅 소켓 연결');
-   socket.on('enterChatRoom', roomId => {
-      socket.join(String(roomId));
-   });
+const chatSocket = wsServer.of("/chat");
+chatSocket.on("connection", (socket: Socket) => {
+  console.log("채팅 소켓 연결");
+  socket.on("enterChatRoom", (roomId) => {
+    socket.join(String(roomId));
+  });
 
-   socket.on('sendMessage', (roomId: number, data: { text: string; created: Date; senderId: number }) => {
-      socket.to(String(roomId)).emit('newChatMessage', data);
-   });
-   socket.on('leaveChatRoom', roomId => {
-      socket.leave(String(roomId));
-   });
+  socket.on("sendMessage", (roomId: number, data: { text: string; created: Date; senderId: number }) => {
+    socket.to(String(roomId)).emit("newChatMessage", data);
+    socket.emit("newMessageAlarm", roomId, data);
+  });
+  socket.on("leaveChatRoom", (roomId) => {
+    socket.leave(String(roomId));
+  });
 });
 export default chatSocket;
