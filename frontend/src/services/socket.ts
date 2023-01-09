1c946dc543259244256a59c9edb849c36272ae87
이진희 <speaker1403@gmail.com>
2023-01-09 17:46:54 +0900

    ready to deploy

--- frontend/src/services/socket.ts
+++ frontend/src/services/socket.ts
@@ -1,8 +1,8 @@
 import io from 'socket.io-client';
+const BASEURL = 'rechu.jinytree.shop';
+const socket = io(`${BASEURL}`, { transports: ['websocket'] });
 
-const socket = io('localhost:8080', { transports: ['websocket'] });
-
-const chatSocket = io('localhost:8080/chat', { transports: ['websocket'] });
+const chatSocket = io(`${BASEURL}/chat`, { transports: ['websocket'] });
 
 export default socket;
 
